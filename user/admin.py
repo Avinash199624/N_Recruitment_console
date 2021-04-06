f796856b4cdefe83b1f883781ea7e1944790e321
mindnervestech <mindnervestech@gmail.com>
2021-04-06 14:59:51 +0530

    User Admin Page

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from user.models import User,UserProfile,Location
 
 # Register your models here.
+
+@register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','username','email','created_at']
+
+@register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','gender','phone_no','mobile_no','status','higher_qualification' ]
+
+
+@register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['address1','address2','address3','city','state','country','postcode' ]
