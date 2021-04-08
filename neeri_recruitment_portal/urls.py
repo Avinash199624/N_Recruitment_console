2726bf36e50e90a886ef7ff7fa8c23822f62fb0e
mindnervestech <mindnervestech@gmail.com>
2021-04-08 19:55:37 +0530

    Url fix

--- neeri_recruitment_portal/urls.py
+++ neeri_recruitment_portal/urls.py
@@ -7,5 +7,6 @@ from django.urls import path,include
 urlpatterns = [
     path('user/', include('user.urls')),
     path('document/', include('document.urls')),
+    path('template/', include('communication_template.urls')),
     path('admin/', admin.site.urls),
 ]
