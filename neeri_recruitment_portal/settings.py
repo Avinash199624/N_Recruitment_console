dfc147330eca848841ab1a89b142a9e6d7a97897
shreyasinfo-surajgupta <suraj.gupta@shreyasinfo.co.in>
2021-05-07 16:27:49 +0530

    Updated Database name wrt in settings.py file.

--- neeri_recruitment_portal/settings.py
+++ neeri_recruitment_portal/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'neeri_recruitment_portal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'neeri-live',
+        'NAME': 'neeri-live-qa',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost',
