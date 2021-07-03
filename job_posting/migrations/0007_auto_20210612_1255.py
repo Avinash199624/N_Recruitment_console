f91652cc6d725d9f08ed0e6c36e69924fe6721de
sourabh28 <sourabhbanerjee.28@gmail.com>
2021-07-03 21:10:16 +0530

    making jobposting migrations consistent

--- job_posting/migrations/0007_auto_20210612_1255.py
+++ job_posting/migrations/0007_auto_20210612_1255.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('document', '0002_delete_documentmaster'),
         ('job_posting', '0006_remove_jobposting_department'),
     ]
 
