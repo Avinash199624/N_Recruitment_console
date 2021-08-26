07750985782fbd89beec7e794244ee28ce61c938
Delta <80676284+shreyasinfo-surajgupta@users.noreply.github.com>
2021-08-26 12:48:02 +0530

    Update settings.py

--- neeri_recruitment_portal/settings.py
+++ neeri_recruitment_portal/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
