236f1b931aeaa1ac6b4d75326817a9ebd3d628be
Narendra041 <narendra.nagar@SHREYASINFO.CO.IN>
2021-09-01 13:08:03 +0530

    fixed user profile religion

--- user/models.py
+++ user/models.py
@@ -214,12 +214,12 @@ class UserProfile(BaseModel):
         related_name="father_address",
     )
     father_occupation = models.CharField(max_length=30, null=True, blank=True)
-    religion = models.ForeignKey(
+    user_religion = models.ForeignKey(
         "user.ReligionMaster",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name="user_religion",
+        related_name="u_religion",
     )
     caste = models.CharField(
         max_length=30, choices=CASTE_CHOICES, null=True, blank=True
